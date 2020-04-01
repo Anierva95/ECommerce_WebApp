f5827ca23b0d580fa632892da6702ca519657f4b
Henry Ni <henryni914@gmail.com>
2020-04-01 11:06:59 -0400

    created global state and able to render products from global

--- client/src/components/ProductRow.js
+++ client/src/components/ProductRow.js
@@ -1,33 +1,40 @@
-import React, {useEffect, useState} from 'react';
+import React, { useEffect, useState } from 'react';
 import MediaCard from '../components/Card';
 import { Grid } from '@material-ui/core';
 import API from "../utils/API";
+import { useStoreContext } from "../utils/GlobalState";
 
 
 export default function ProductRow() {
 
-  const [products, setProducts] = useState([])
+  const [state, dispatch] = useStoreContext();
 
   useEffect(() => {
     loadProducts()
   }, [])
 
   function loadProducts() {
-    API.getProducts().then(res => setProducts(res.data))
+    API.getProducts().then(res => {
+      dispatch({
+        type: "GET_PRODUCTS",
+        products: res.data
+      })
+    })
+    .catch(err => console.log(err));
   }
-  
+
   return (
     <>
-    {products.map(product => (
-       <Grid item xs={3}>
-       <MediaCard 
-       name = {product.Item}
-       description = {product.Description}
-       price= {product.Price}
-       />
-     </Grid>
-    ))}
-     
+      {state.products.map(product => (
+        <Grid item xs={3}>
+          <MediaCard
+            name={product.Item}
+            description={product.Description}
+            price={product.Price}
+          />
+        </Grid>
+      ))}
+
     </>
   )
 }
--- client/src/utils/GlobalState.js
+++ client/src/utils/GlobalState.js
@@ -1,16 +1,39 @@
-import React, {createContext, useReducer, useContext } from "react";
+import React, { createContext, useReducer, useContext } from "react";
 
 const StoreContext = createContext();
 const { Provider } = StoreContext;
 
-// const reducer = (state, action) => {
-//     console.log(`reducer state`, state);
-//     console.log(`reducer state`, action);
+const reducer = (state, action) => {
+    console.log(`reducer state`, state);
+    console.log(`reducer state`, action);
 
-//     switch (action.type) {
-//         case "GET_PRODUCTS":
-//             return {
-//                 ...state
-//             }
-//     }
-// }
+    switch (action.type) {
+        case "GET_PRODUCTS":
+            return {
+                ...state,
+                products: [...action.products]
+            }
+    }
+}
+
+const StoreProvider = ({ value = [], ...props }) => {
+    const [state, dispatch] = useReducer(reducer, {
+        products: [],
+        shoppingCart: [],
+        wishList: [],
+        blostPosts: [],
+        currentUser: {
+            title: "",
+            body: "",
+            author: ""
+        },
+    });
+
+    return <Provider value={[state, dispatch]} {...props} />;
+};
+
+const useStoreContext = () => {
+    return useContext(StoreContext);
+};
+
+export { StoreProvider, useStoreContext };
