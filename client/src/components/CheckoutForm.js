93fb7a1ce414c9252382bb1b2157e5c6f9982ae6
Henry Ni <henryni914@gmail.com>
2020-04-02 15:08:03 -0400

    commented out all stripe parts

--- client/src/components/CheckoutForm.js
+++ client/src/components/CheckoutForm.js
@@ -1,144 +1,144 @@
-import React, { useEffect, useState } from "react";
-import { CardElement, useStripe, useElements } from "@stripe/react-stripe-js";
-import "./CheckoutForm.css";
-import api from "../api";
+// import React, { useEffect, useState } from "react";
+// import { CardElement, useStripe, useElements } from "@stripe/react-stripe-js";
+// import "./CheckoutForm.css";
+// import api from "../api";
 
-export default function CheckoutForm() {
-  const [amount, setAmount] = useState(0);
-  const [currency, setCurrency] = useState("");
-  const [clientSecret, setClientSecret] = useState(null);
-  const [error, setError] = useState(null);
-  const [metadata, setMetadata] = useState(null);
-  const [succeeded, setSucceeded] = useState(false);
-  const [processing, setProcessing] = useState(false);
-  const stripe = useStripe();
-  const elements = useElements();
+// export default function CheckoutForm() {
+//   const [amount, setAmount] = useState(0);
+//   const [currency, setCurrency] = useState("");
+//   const [clientSecret, setClientSecret] = useState(null);
+//   const [error, setError] = useState(null);
+//   const [metadata, setMetadata] = useState(null);
+//   const [succeeded, setSucceeded] = useState(false);
+//   const [processing, setProcessing] = useState(false);
+//   const stripe = useStripe();
+//   const elements = useElements();
 
-  useEffect(() => {
+//   useEffect(() => {
 
-    api.getProductDetails().then(productDetails => {
-      setAmount(productDetails.amount / 100);
-      setCurrency(productDetails.currency);
-    });
+//     api.getProductDetails().then(productDetails => {
+//       setAmount(productDetails.amount / 100);
+//       setCurrency(productDetails.currency);
+//     });
 
-    api
-      .createPaymentIntent({
-        payment_method_types: ["card"]
-      })
-      .then(clientSecret => {
-        setClientSecret(clientSecret);
-      })
-      .catch(err => {
-        setError(err.message);
-      });
-  }, []);
+//     api
+//       .createPaymentIntent({
+//         payment_method_types: ["card"]
+//       })
+//       .then(clientSecret => {
+//         setClientSecret(clientSecret);
+//       })
+//       .catch(err => {
+//         setError(err.message);
+//       });
+//   }, []);
 
-  const handleSubmit = async event => {
-    event.preventDefault();
-    setProcessing(true);
+//   const handleSubmit = async event => {
+//     event.preventDefault();
+//     setProcessing(true);
 
 
-    const payload = await stripe.confirmCardPayment(clientSecret, {
-      payment_method: {
-        card: elements.getElement(CardElement),
-        billing_details: {
-          name: event.target.name.value
-        }
-      }
-    });
+//     const payload = await stripe.confirmCardPayment(clientSecret, {
+//       payment_method: {
+//         card: elements.getElement(CardElement),
+//         billing_details: {
+//           name: event.target.name.value
+//         }
+//       }
+//     });
 
-    if (payload.error) {
-      setError(`Payment failed: ${payload.error.message}`);
-      setProcessing(false);
-      console.log("[error]", payload.error);
-    } else {
-      setError(null);
-      setSucceeded(true);
-      setProcessing(false);
-      setMetadata(payload.paymentIntent);
-      console.log("[PaymentIntent]", payload.paymentIntent);
-    }
-  };
+//     if (payload.error) {
+//       setError(`Payment failed: ${payload.error.message}`);
+//       setProcessing(false);
+//       console.log("[error]", payload.error);
+//     } else {
+//       setError(null);
+//       setSucceeded(true);
+//       setProcessing(false);
+//       setMetadata(payload.paymentIntent);
+//       console.log("[PaymentIntent]", payload.paymentIntent);
+//     }
+//   };
 
-  const renderSuccess = () => {
-    return (
-      <div className="sr-field-success message">
-        <h1>Your test payment succeeded</h1>
-        <p>View PaymentIntent response:</p>
-        <pre className="sr-callout">
-          <code>{JSON.stringify(metadata, null, 2)}</code>
-        </pre>
-      </div>
-    );
-  };
+//   const renderSuccess = () => {
+//     return (
+//       <div className="sr-field-success message">
+//         <h1>Your test payment succeeded</h1>
+//         <p>View PaymentIntent response:</p>
+//         <pre className="sr-callout">
+//           <code>{JSON.stringify(metadata, null, 2)}</code>
+//         </pre>
+//       </div>
+//     );
+//   };
 
-  const renderForm = () => {
-    const options = {
-      style: {
-        base: {
-          color: "#32325d",
-          fontFamily: '"Helvetica Neue", Helvetica, sans-serif',
-          fontSmoothing: "antialiased",
-          fontSize: "16px",
-          "::placeholder": {
-            color: "#aab7c4"
-          }
-        },
-        invalid: {
-          color: "#fa755a",
-          iconColor: "#fa755a"
-        }
-      }
-    };
+//   const renderForm = () => {
+//     const options = {
+//       style: {
+//         base: {
+//           color: "#32325d",
+//           fontFamily: '"Helvetica Neue", Helvetica, sans-serif',
+//           fontSmoothing: "antialiased",
+//           fontSize: "16px",
+//           "::placeholder": {
+//             color: "#aab7c4"
+//           }
+//         },
+//         invalid: {
+//           color: "#fa755a",
+//           iconColor: "#fa755a"
+//         }
+//       }
+//     };
 
-    return (
-      <form onSubmit={handleSubmit}>
-        <h1>
-          {currency.toLocaleUpperCase()}{" "}
-          {amount.toLocaleString(navigator.language, {
-            minimumFractionDigits: 2
-          })}{" "}
-        </h1>
-        <h4>Pre-order of zhaoyang</h4>
+//     return (
+//       <form onSubmit={handleSubmit}>
+//         <h1>
+//           {currency.toLocaleUpperCase()}{" "}
+//           {amount.toLocaleString(navigator.language, {
+//             minimumFractionDigits: 2
+//           })}{" "}
+//         </h1>
+//         <h4>Pre-order of zhaoyang</h4>
 
-        <div className="sr-combo-inputs">
-          <div className="sr-combo-inputs-row">
-            <input
-              type="text"
-              id="name"
-              name="name"
-              placeholder="Name"
-              autoComplete="cardholder"
-              className="sr-input"
-            />
-          </div>
+//         <div className="sr-combo-inputs">
+//           <div className="sr-combo-inputs-row">
+//             <input
+//               type="text"
+//               id="name"
+//               name="name"
+//               placeholder="Name"
+//               autoComplete="cardholder"
+//               className="sr-input"
+//             />
+//           </div>
 
-          <div className="sr-combo-inputs-row">
-            <CardElement
-              className="sr-input sr-card-element"
-              options={options}
-            />
-          </div>
-        </div>
+//           <div className="sr-combo-inputs-row">
+//             <CardElement
+//               className="sr-input sr-card-element"
+//               options={options}
+//             />
+//           </div>
+//         </div>
 
-        {error && <div className="message sr-field-error">{error}</div>}
+//         {error && <div className="message sr-field-error">{error}</div>}
 
-        <button
-          className="btn"
-          disabled={processing || !clientSecret || !stripe}
-        >
-          {processing ? "Processing…" : "Pay"}
-        </button>
-      </form>
-    );
-  };
+//         <button
+//           className="btn"
+//           disabled={processing || !clientSecret || !stripe}
+//         >
+//           {processing ? "Processing…" : "Pay"}
+//         </button>
+//       </form>
+//     );
+//   };
 
-  return (
-    <div className="checkout-form">
-      <div className="sr-payment-form">
-        <div className="sr-form-row" />
-        {succeeded ? renderSuccess() : renderForm()}
-      </div>
-    </div>
-  );
-}
+//   return (
+//     <div className="checkout-form">
+//       <div className="sr-payment-form">
+//         <div className="sr-form-row" />
+//         {succeeded ? renderSuccess() : renderForm()}
+//       </div>
+//     </div>
+//   );
+// }
