d7e5d7559b5dae23d3200712802bf111d09a5792
Anierva95 <46223963+Anierva95@users.noreply.github.com>
2020-04-02 13:21:55 -0400

    login button redirects properly

--- client/src/pages/Home.js
+++ client/src/pages/Home.js
@@ -1,13 +1,17 @@
-import React from 'react';
+import React, {useContext} from 'react';
 import Navbar from '../components/Navbar';
 import SalesHeader from '../components/SalesHeader'
 import ProductRow from '../components/ProductRow'
 import { Grid } from '@material-ui/core';
+import { Auth0Context } from '../utils/auth0context';
+import Button from '@material-ui/core/Button';
+
 // import Cart from '../components/Cart';
 // import CartItem from '../components/CartItem'
 // import React, { useState, data } from 'react';
 
 export default function Home(){
+  const auth0 = useContext(Auth0Context)
   // const [products] = useState(data);
 	// const [cart, setCart] = useState([]);
 
@@ -19,6 +23,9 @@ export default function Home(){
           <Navbar/>
 
           <SalesHeader/>
+          <Button variant="contained" color="primary" onClick={auth0.loginWithRedirect} className="button is-danger">
+            Login
+          </Button>
           <Grid container direction="row">
           <Grid item xs={2} />
           <Grid item container direction="row" xs={8}>
