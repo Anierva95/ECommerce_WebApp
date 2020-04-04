bfefe0115e4f4ba0f72b2602841808d520dc7944
Anierva95 <46223963+Anierva95@users.noreply.github.com>
2020-04-04 19:19:35 -0400

    all files cleaned but auth0 is a bum

--- client/src/App.js
+++ client/src/App.js
@@ -1,7 +1,6 @@
 import React, {useContext} from 'react';
 import Home from './pages/Home';
 import Blog from './pages/Blog';
-import CheckoutPage from './pages/Checkoutpage'
 import StoreManager from './pages/StoreManager';
 import Cart from './pages/Cart'
 import { BrowserRouter as Router, Route } from "react-router-dom";
@@ -24,7 +23,6 @@ function App() {
           <Route exact path="/" component={Home} />
           <Route exact path="/blog" component={Blog} />
           <Route exact path="/storeManager" component={StoreManager} />
-          <Route exact path="/CheckoutPage" component={CheckoutPage} />
           <Route exact path="/cart" component={Cart} />
         </StoreProvider>
         </Auth0Provider>
