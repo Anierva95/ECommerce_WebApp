54f9dac564f712f43df560dde90e4f57fc2412a7
Zhaoyang Xia <zx70@scarletmail.rutgers.edu>
2020-04-05 15:50:04 -0400

    chat

--- client/src/pages/Home.js
+++ client/src/pages/Home.js
@@ -3,6 +3,7 @@ import Navbar from '../components/Navbar';
 import SalesHeader from '../components/SalesHeader'
 import ProductRow from '../components/ProductRow'
 import { Grid } from '@material-ui/core';
+import LiveChat from 'react-livechat'
 
 
 export default function Home(){
@@ -19,6 +20,7 @@ export default function Home(){
           <ProductRow/>
           <Grid item xs={2} />
           </Grid>
+          <LiveChat license={11844840}/>
           </Grid>
 
         </div>
