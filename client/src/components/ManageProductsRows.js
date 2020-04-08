7c2d2f730d68386fed76f00d79e30224ffa9e54b
Anierva95 <46223963+Anierva95@users.noreply.github.com>
2020-04-08 09:19:20 -0400

    delete item works

--- client/src/components/ManageProductsRows.js
+++ client/src/components/ManageProductsRows.js
@@ -21,7 +21,7 @@ export default function ManageProductRows() {
 
   useEffect(() => {
     loadProducts()
-  }, []);
+  }, [state.products]);
 
   return (
     <>
--- client/src/utils/GlobalState.js
+++ client/src/utils/GlobalState.js
@@ -50,6 +50,7 @@ const reducer = (state, action) => {
                 ...state,
                 shoppingCart: action.dbCart
             }
+        
     }
 }
 
